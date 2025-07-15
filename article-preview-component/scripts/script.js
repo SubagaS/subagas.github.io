2627c05bcc13f28c589ee3d11cc849320232dee3
Subaga <subaga98@gmail.com>
2025-07-15 16:50:24 +0200

    Initial-commit

---
+++ article-preview-component/scripts/script.js
@@ -0,0 +1,14 @@
+const shareProfileBtn = document.querySelector('.share-icon-profile');
+const shareLinkBtn = document.querySelector('.share-icon-links');
+const profileLinkContainer = document.querySelector('.profile-links');
+
+function showLinks() {
+  console.log('heyy');
+  profileLinkContainer.classList.toggle('hidden');
+}
+function hideLinks() {
+  profileLinkContainer.classList.toggle('hidden');
+}
+
+shareProfileBtn.addEventListener('click', showLinks);
+shareLinkBtn.addEventListener('click', hideLinks);
